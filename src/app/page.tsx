c1f3c233933ba9aa8237b023e44cae6c33eebcc7
Benjamin Shuster <77933963+bentzi-shuster@users.noreply.github.com>
2024-02-06 16:37:49 -0500

    Remove unused CSS and update login/logout functionality

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,16 @@
 import Image from "next/image";
-import styles from "./page.module.css";
 import { getSession } from "@auth0/nextjs-auth0";
 
 export default async function Home() {
   const  session = await getSession();
+  let loggedIn = !!(session && session.user && session.user["name"]);
   return (
-    <main className={styles.main}>
-<a href="/api/auth/login"><h1>LOGIN</h1></a>
-<a href="/api/auth/logout">LOGOUT</a>
+    <main>
+      <h1>You are logged {loggedIn ? "In" : "Out"}</h1>
+      {
+        loggedIn ? <a href="/api/auth/logout"><h1>LOGOUT</h1></a>:<a href="/api/auth/login"><h1>LOGIN</h1></a>
+      }
+
     </main>
   );
 }
